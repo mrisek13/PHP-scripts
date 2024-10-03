47689ce591edcc3d6c58b44f30d0e2c77e79da7b
Maja Risek <108302992+mrisek13@users.noreply.github.com>
2024-10-03 22:38:44 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<body>
+ 
+<?php
+echo "My first PHP script!";
+$txt = "W3Schools.com";
+echo "I love $txt!";
+$txt2 = "W3Schools.com";
+echo "I love " . $txt2 . "!";
+?>
+
+<p>Hello, world!</p>
+
+</body>
+</html>
